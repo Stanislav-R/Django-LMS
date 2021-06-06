65709c1f7f9e156a33521402d4d5268b8908de8d
Stanislav <stanislav.rudoi@gmail.com>
2021-06-06 20:18:35 +0300

    HW7_Django on review

---
+++ Lessons/teachers/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    position = models.CharField(max_length=80, null=True)
+    work_experience = models.IntegerField(default=10)
