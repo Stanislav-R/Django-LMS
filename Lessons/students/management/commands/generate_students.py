b1f0e6f3dd26c9b61de175d10ec2c873b6b39d8e
Stanislav <stanislav.rudoi@gmail.com>
2021-06-11 12:36:37 +0300

    Command generate_students updated

--- Lessons/students/management/commands/generate_students.py
+++ Lessons/students/management/commands/generate_students.py
@@ -11,14 +11,4 @@ class Command(BaseCommand):
         parser.add_argument('count', type=int, help='Indicates the number of students to be created')
 
     def handle(self, *args, **options):
-        count = options['count']
-        faker = Faker()
-        for _ in range(count):
-            st = Student(
-                first_name=faker.first_name(),
-                last_name=faker.last_name(),
-                email=faker.email(),
-                birthdate=faker.date_between(start_date='-65y', end_date='-18y')
-            )
-
-            st.save()
+        return Student.generate_students(count=options['count'])
